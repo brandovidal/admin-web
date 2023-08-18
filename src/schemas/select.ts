3c67b3e8feb77b1d2eb6b27661ac3d2a9c64cc47
Brando Vidal <yisusdracon@gmail.com>
2023-08-18 04:11:03 -0500

    ⚒  remove select schema codes

--- src/schemas/select.ts
+++ src/schemas/select.ts
@@ -1,23 +1,8 @@
 
 import { z } from 'zod'
-// import isEmpty from 'just-is-empty'
 
 export const SelectSchema = z.object({
   id: z.string({ required_error: 'Enter your ID.' }).nullish(),
   label: z.string({ required_error: 'Enter your label.' }).nullable(),
   value: z.string({ required_error: 'Enter your value.' }).nullable()
-})
-  // .nullish()
-  // .superRefine((arg, ctx) => {
-  //   console.log('🚀 ~ file: select.ts:13 ~ .superRefine ~ arg:', arg)
-  //   if (isEmpty(arg)) {
-  //     ctx.addIssue({
-  //       code: z.ZodIssueCode.custom,
-  //       message: 'Select a value.'
-  //     })
-  //   }
-  //   const val = arg?.value
-  //   console.log('🚀 ~ file: select.ts:21 ~ .superRefine ~ val:', val)
-  //   return z.INVALID
-  // })
-  .transform(arg => arg?.value)
+}).transform(arg => arg?.value)
